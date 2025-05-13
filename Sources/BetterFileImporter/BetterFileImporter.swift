dedbe85b866899c6e8d8f4948e8b99877967a312
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-05-13 15:45:07 +0200

    Update BetterFileImporter.swift

--- Sources/BetterFileImporter/BetterFileImporter.swift
+++ Sources/BetterFileImporter/BetterFileImporter.swift
@@ -54,46 +54,51 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 public struct FileImporterRepresentableView: UIViewControllerRepresentable {
-	public var allowedContentTypes: [UTType]
-	public var allowsMultipleSelection: Bool
-	public var onDocumentsPicked: ([URL]) -> Void
-	
-	public init(
-		allowedContentTypes: [UTType],
-		allowsMultipleSelection: Bool,
-		onDocumentsPicked: @escaping ([URL]) -> Void
-	) {
-		self.allowedContentTypes = allowedContentTypes
-		self.allowsMultipleSelection = allowsMultipleSelection
-		self.onDocumentsPicked = onDocumentsPicked
-	}
-	
-	public func makeCoordinator() -> Coordinator {
-		Coordinator(onDocumentsPicked: onDocumentsPicked)
-	}
-	
-	public func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
-		let picker = UIDocumentPickerViewController(forOpeningContentTypes: allowedContentTypes, asCopy: allowedContentTypes.contains(.folder) ? false : true)
-		picker.delegate = context.coordinator
-		picker.allowsMultipleSelection = allowsMultipleSelection
-		return picker
-	}
-	
-	public func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
-	
-	public class Coordinator: NSObject, UIDocumentPickerDelegate {
-		var onDocumentsPicked: ([URL]) -> Void
-		
-		init(onDocumentsPicked: @escaping ([URL]) -> Void) {
-			self.onDocumentsPicked = onDocumentsPicked
-		}
-		
-		public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-			onDocumentsPicked(urls)
-		}
-		
-		public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-			onDocumentsPicked([])
-		}
-	}
+    public var allowedContentTypes: [UTType]
+    public var allowsMultipleSelection: Bool
+    public var onDocumentsPicked: ([URL]) -> Void
+    
+    public init(
+        allowedContentTypes: [UTType],
+        allowsMultipleSelection: Bool,
+        onDocumentsPicked: @escaping ([URL]) -> Void
+    ) {
+        self.allowedContentTypes = allowedContentTypes
+        self.allowsMultipleSelection = allowsMultipleSelection
+        self.onDocumentsPicked = onDocumentsPicked
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(onDocumentsPicked: onDocumentsPicked)
+    }
+    
+    public func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: allowedContentTypes, asCopy: allowedContentTypes.contains(.folder) ? false : true)
+        picker.delegate = context.coordinator
+        picker.allowsMultipleSelection = allowsMultipleSelection
+        return picker
+    }
+    
+    public func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+    
+    public class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var onDocumentsPicked: ([URL]) -> Void
+        
+        init(onDocumentsPicked: @escaping ([URL]) -> Void) {
+            self.onDocumentsPicked = onDocumentsPicked
+        }
+        
+        public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                self.onDocumentsPicked(urls)
+            }
+        }
+        
+        public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                self.onDocumentsPicked([])
+            }
+        }
+        
+    }
 }
